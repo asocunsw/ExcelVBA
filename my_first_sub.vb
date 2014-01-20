c72cb965c64f92412f61c983374ed0778937c5c3
Prem Rajendran <premrajendran1@gmail.com>
2014-01-20 17:10:24 +1100

    fixed my_first_sub

---
+++ my_first_sub.vb
@@ -0,0 +1,14 @@
+' My first Excel VBA program
+' Opens a message box with the text "Hello World!"
+'
+' Excel VBA instructions are enclosed within a subroutine or Sub for short
+' We have named our subroutine my_first_sub but this could be anything
+' Any line that starts with a ' is a comment and is not read by the computer
+ 
+Sub my_first_sub()
+    
+    ' Change the text within the double quotes
+    MsgBox ("Hello World!")
+    
+End Sub
+
